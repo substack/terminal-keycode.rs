3d91cfdf8852cce3f98b1e718fc351462e28c90f
substack <substack@bits.coop>
2022-03-21 02:23:18 -1000

    more tests for bytes

--- tests/keys.rs
+++ tests/keys.rs
@@ -19,4 +19,48 @@ fn keys() {
   assert_eq![decoder.write(0xeb), vec![]];
   assert_eq![decoder.write(0xa5), vec![]];
   assert_eq![decoder.write(0x1b), vec![KeyCode::Byte(0xeb),KeyCode::Byte(0xa5),KeyCode::Byte(0x1b)]];
+  assert_eq![decoder.write(0x1b), vec![]];
+  assert_eq![decoder.write(0x5b), vec![]];
+  assert_eq![decoder.write(0x31), vec![]];
+  assert_eq![decoder.write(0x35), vec![]];
+  assert_eq![decoder.write(0x70), vec![
+    KeyCode::Escape, KeyCode::Char('['), KeyCode::Char('1'), KeyCode::Char('5'), KeyCode::Char('p')
+  ]];
+}
+
+#[test]
+fn bytes() {
+  let mut decoder = Decoder::new();
+  let keycodes = vec![
+    KeyCode::ArrowUp, KeyCode::ArrowDown, KeyCode::ArrowLeft, KeyCode::ArrowRight,
+    KeyCode::Numpad5, KeyCode::Delete, KeyCode::Insert, KeyCode::Home, KeyCode::End,
+    KeyCode::PageUp, KeyCode::PageDown,
+    KeyCode::F1, KeyCode::F2, KeyCode::F3, KeyCode::F4, KeyCode::F5, KeyCode::F6,
+    KeyCode::F7, KeyCode::F8, KeyCode::F9, KeyCode::F10, KeyCode::F11, KeyCode::F12,
+    KeyCode::CtrlA, KeyCode::CtrlB, KeyCode::CtrlC, KeyCode::CtrlD, KeyCode::CtrlE,
+    KeyCode::CtrlF, KeyCode::CtrlG, KeyCode::CtrlH,
+    KeyCode::Tab, KeyCode::Linefeed, KeyCode::Enter,
+    KeyCode::CtrlK, KeyCode::CtrlL, KeyCode::CtrlN, KeyCode::CtrlO, KeyCode::CtrlP,
+    KeyCode::CtrlQ, KeyCode::CtrlR, KeyCode::CtrlS, KeyCode::CtrlT, KeyCode::CtrlU,
+    KeyCode::CtrlV, KeyCode::CtrlW, KeyCode::CtrlX, KeyCode::CtrlY, KeyCode::CtrlZ,
+    KeyCode::Space, KeyCode::Backspace,
+    KeyCode::Char('a'),
+    KeyCode::Char('B'),
+    KeyCode::Char('t'),
+    KeyCode::Char('U'),
+    KeyCode::Char('€'),
+    KeyCode::Char('ø'),
+    KeyCode::Char('☃'),
+  ];
+  for keycode in keycodes {
+    let bs = keycode.bytes();
+    let len = bs.len();
+    for (i,b) in bs.iter().enumerate() {
+      if i == len-1 {
+        assert_eq![decoder.write(*b), vec![keycode.clone()]];
+      } else {
+        assert_eq![decoder.write(*b), vec![]];
+      }
+    }
+  }
 }
